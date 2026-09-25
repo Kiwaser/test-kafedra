08b4f48c3748e60765dadb64ef19e68af16c4e9e
Kiwaser <egort011t@gmail.com>
2026-09-25 11:24:09 +0700

    Release deploy Docker

---
+++ deploy/script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+docker compose exec php composer update
+
+docker compose exec php composer install
+
+docker compose exec php php artisan key:generate
+
+docker compose exec php php artisan migrate
+
+
+
+
+
+
+
+docker compose exec php php artisan migrate:fresh --seed
