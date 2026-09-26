7e3c5485c4c07e2ef23695a655a73cbd3b051511
Kiwaser <egort011t@gmail.com>
2026-09-26 22:32:24 +0700

    Release README and bugfix (Errors related to returning 403 and 500 status codes when the data is correct have been fixed.)

--- deploy/script.sh
+++ deploy/script.sh
@@ -6,12 +6,4 @@ docker compose exec php composer install
 
 docker compose exec php php artisan key:generate
 
-docker compose exec php php artisan migrate
-
-
-
-
-
-
-
 docker compose exec php php artisan migrate:fresh --seed
